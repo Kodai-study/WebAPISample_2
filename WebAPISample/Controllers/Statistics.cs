a3c90f9146cad558a9ea256e6b93f3233673ec3d
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2022-12-03 18:09:31 +0900

    検査結果の統計情報を管理するやつを追加

---
+++ WebAPISample/Controllers/Statistics.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Primitives;
+using WebAPISample.Models;
+
+namespace WebAPISample.Controllers
+{
+    [Route("api/insert")]
+    [ApiController]
+    public class Statistics : ControllerBase
+    {
+        
+        [HttpGet]
+        public void get([FromQuery] int num)
+        {
+            StringValues val = new StringValues("*");
+            this.Response.Headers.Add("Access-Control-Allow-Origin", val);
+
+            var date_sql = "SELECT YMD,scan,OK,ave_temp,ave_hun,ave_illum,ave_cyctime,max_cyctime,min_cyctime FROM View_DOK";
+            
+            var all_sql = "SELECT SUM(scan) AS al,SUM(OK) AS ok,100.0 * SUM(OK) / SUM(scan) FROM View_DOK";
+        }
+    }
+}
---
+++ WebAPISample/Models/Statistics.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Primitives;
+using WebAPISample.Models;
+
+namespace WebAPISample.Modules
+{
+    public class Statistics : ControllerBase
+    {
+        public DateOnly YMD { get; set; }
+        public int Scan { get; set; }
+        public int OK { get; set; }
+        public float ave_temp { get; set; }
+        public float ave_hum { get; set; }
+        public float ave_illum { get; set; }
+        public TimeSpan ave_cycletime { get; set; }
+        public TimeSpan min_cycletime { get; set; }
+        public TimeSpan max_cycletime { get; set; }
+
+        public Statistics(DateOnly YMD, int Scan, int OK, float ave_temp, float ave_hum,
+           float ave_illum, float ave_cycletime, int ave_cycletime_sec)
+        {
+            this.YMD = TMD;
+            this.OK = OK;
+            this.ave_temp = ave_temp;
+            this.ave_hum = ave_hum;
+            this.ave_illum = ave_illum;
+            this.ave_cycletime = ave_cycletime;
+        }
+    }
+}
