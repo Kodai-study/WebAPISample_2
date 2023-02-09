1ba9b0ccd1a2176bca6c988c0f84db63fd2bd314
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2023-02-09 14:19:15 +0900

    ロボットの状態変化時間を読み取って、運転状態にいた時間と停止していた時間の合計をそれぞれ出す

---
+++ WebAPISample/Controllers/UtilizationControll.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Primitives;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+using WebAPISample.Models;
+
+namespace WebAPISample.Controllers
+{
+    [Route("api/stationUtilization")]
+    [ApiController]
+    public class UtilizationControll : ControllerBase
+    {
+        public const String STATECODE_START = "Start";
+        public const String STATECODE_END = "End";
+        public const String STATECODE_PAUSE = "Pause";
+        public const String STATECODE_RESTART = "Restart";
+        public const String STATECODE_EMERGENCY = "Emergency";
+        public const String STATECODE_STANDBY = "Standby";
+
+
+        [HttpGet]
+        public List<Utilization> Get([FromQuery] TimeParams timeParams)
+        {
+            List<Utilization> utilizationList = new List<Utilization>();
+
+
+
+            StringValues val = new StringValues("*");
+            this.Response.Headers.Add("Access-Control-Allow-Origin", val);
+            StringBuilder sql = new StringBuilder("SELECT state_Code,time FROM StateTimeT");
+
+            if (timeParams)
+            {
+                sql.Append(" WHERE supply ");
+                sql.Append(timeParams.CreateSQL());
+            }
+            using (var command = new SqlCommand(sql.ToString(), Parameters.sqlConnection))
+            using (var reader = command.ExecuteReader())
+            {
+                List<Tuple<String, DateTime>> stateChangeTimes =
+                   new List<Tuple<string, DateTime>>();
+
+
+                DateTime currentDate;
+                if (reader.Read())
+                {
+                    currentDate = reader.GetDateTime(1).Date;
+                    stateChangeTimes.Add(
+                        new Tuple<string, DateTime>(reader.GetString(0), reader.GetDateTime(1)));
+                }
+                else
+                {
+                    Console.WriteLine("ロボットの状態変化テーブルが読み取れなかった");
+                    return utilizationList;
+                }
+                while (reader.Read())
+                {
+                    if (reader.GetDateTime(1).Date > currentDate)
+                    {
+                        utilizationList.Add(new Utilization(currentDate, stateChangeTimes));
+                        stateChangeTimes = new List<Tuple<string, DateTime>>();
+                        currentDate = currentDate.AddDays(1);
+                    }
+                    stateChangeTimes.Add(
+                        new Tuple<string, DateTime>(reader.GetString(0), reader.GetDateTime(1)));
+                }
+                utilizationList.Add(new Utilization(currentDate, stateChangeTimes));
+                return utilizationList;
+            }
+        }
+    }
+}
---
+++ WebAPISample/Models/Utilization.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO.MemoryMappedFiles;
+using WebAPISample.Models;
+
+namespace WebAPISample.Controllers
+{
+    public class Utilization
+    {
+        public DateTime currentDate { get; set; } = DateTime.MinValue;
+        public TimeSpan timeOfOperation { get; set; } = TimeSpan.Zero;
+        public TimeSpan timeOfStopSum { get; set; } = TimeSpan.Zero;
+
+        public TimeSpan timeOfSupplyPause { get; set; } = TimeSpan.Zero;
+        public TimeSpan timeOfPause { get; set; } = TimeSpan.Zero;
+
+        public Utilization() { }
+
+        public Utilization(DateTime currentDate, List<Tuple<String, DateTime>> stateChangeDatas)
+        {
+            DateTime startTime = DateTime.MinValue;
+            DateTime pauseTime = DateTime.MinValue;
+            String lastStopCause = "";
+            this.currentDate = currentDate;
+            foreach (var changeData in stateChangeDatas)
+            {
+                if (changeData.Item1.Equals(UtilizationControll.STATECODE_START))
+                {
+                    startTime = changeData.Item2;
+                }
+                else if (changeData.Item1.Equals(UtilizationControll.STATECODE_END))
+                {
+                    if(startTime.Equals(DateTime.MinValue))
+                    {
+                        Console.WriteLine("ロボットのストップ前にスタートがかかった");
+                        return;
+                    }
+                    timeOfOperation += (changeData.Item2 - startTime);
+                    startTime = DateTime.MinValue;
+                }
+                else if (changeData.Item1.Equals(UtilizationControll.STATECODE_PAUSE))
+                {
+                    lastStopCause = UtilizationControll.STATECODE_PAUSE;
+                    pauseTime = changeData.Item2;
+                }
+                else if (changeData.Item1.Equals(UtilizationControll.STATECODE_STANDBY))
+                {
+                    pauseTime = changeData.Item2;
+                    lastStopCause = UtilizationControll.STATECODE_STANDBY;
+                }
+                else if (changeData.Item1.Equals(UtilizationControll.STATECODE_RESTART))
+                {
+                    if (startTime.Equals(DateTime.MinValue))
+                    {
+                        Console.WriteLine("ロボットのストップ前にリスタートがかかった");
+                        return;
+                    }
+                    if (lastStopCause.Equals(UtilizationControll.STATECODE_PAUSE))
+                    {
+                        timeOfSupplyPause += (changeData.Item2 - pauseTime);
+                    }
+                    else if (lastStopCause.Equals(UtilizationControll.STATECODE_STANDBY))
+                    {
+                        timeOfPause += (changeData.Item2 - pauseTime);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ロボットのストップ前にリスタートがかかった");
+                    }
+                    pauseTime = DateTime.MinValue;
+                }
+                else if (changeData.Item1.Equals(UtilizationControll.STATECODE_EMERGENCY))
+                {
+                    startTime = DateTime.MinValue;
+                    pauseTime = DateTime.MinValue;
+                }
+            }
+        }
+
+    }
+}
