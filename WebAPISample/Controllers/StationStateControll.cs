c965c75b69afbae35007347b16f35970be171c9e
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2023-02-09 10:35:09 +0900

    ステーション毎に、ステーション内にあるワークの数を取得するAPIを作成

---
+++ WebAPISample/Controllers/StationStateControll.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO.MemoryMappedFiles;
+using WebAPISample.Models;
+
+namespace WebAPISample.Controllers
+{
+    [Route("api/stationStatus")]
+    [ApiController]
+    public class StationStateControll : ControllerBase
+    {
+
+        [HttpGet]
+        public StationState Get()
+        {
+            return new StationState();
+        }
+    }
+}
---
+++ WebAPISample/Models/StationState.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO.MemoryMappedFiles;
+
+namespace WebAPISample.Models
+{
+    public class StationState
+    {
+        public int numberOfWork_VisualStation { get; set; }
+        public int numberOfWork_FunctionalStation { get; set; }
+        public int numberOfWork_AsseblyStation { get; set; }
+
+        public StationState()
+        {
+            try
+            {
+                MemoryMappedFile share_mem = MemoryMappedFile.OpenExisting("shared_memory");
+                MemoryMappedViewAccessor accessor = share_mem.CreateViewAccessor();
+
+                if (accessor.CanRead)
+                {
+
+                    numberOfWork_VisualStation = accessor.ReadInt32(0);
+                    numberOfWork_FunctionalStation = accessor.ReadInt32(1);
+                    numberOfWork_AsseblyStation = accessor.ReadInt32(2);
+                }
+                accessor.Dispose();
+                share_mem.Dispose();
+            }
+            catch
+            {
+                numberOfWork_VisualStation = -1;
+                numberOfWork_FunctionalStation = -1;
+                numberOfWork_AsseblyStation = -1;
+            }
+        }
+
+        public StationState(int numberOfWork_VisualStation, int numberOfWork_FunctionalStation, int numberOfWork_AsseblyStation)
+        {
+            this.numberOfWork_VisualStation = numberOfWork_VisualStation;
+            this.numberOfWork_FunctionalStation = numberOfWork_FunctionalStation;
+            this.numberOfWork_AsseblyStation = numberOfWork_AsseblyStation;
+        }
+    }
+}
