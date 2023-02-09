8cf1482709b27f619b3b9d88fa00895a3858ec5c
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2023-02-09 13:13:21 +0900

    1日ごとの検査結果を表示するAPIを作成
    1週間、1か月ごとの結果の中身は1日ごとのデータを使用

---
+++ WebAPISample/Controllers/DailyResultControll.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Primitives;
+using System.Text;
+using WebAPISample.JSONModels;
+using WebAPISample.Query;
+
+namespace WebAPISample.Controllers
+{
+    [Route("api/statistics")]
+    [ApiController]
+    public class DailyResultControll : ControllerBase
+    {
+
+        [HttpGet]
+        public List<DailyResults> Get([FromQuery] TimeRangeParams timeSearch, [FromQuery] String dateTimeKind)
+        {
+            StringValues val = new StringValues("*");
+            this.Response.Headers.Add("Access-Control-Allow-Origin", val);
+
+            String? conditionalSql = null;
+
+            dateTimeKind = dateTimeKind.ToUpper();
+
+            if (timeSearch.IsSetParams)
+            {
+                conditionalSql = " WHERE supply " + timeSearch.CreateSQL();
+            }
+
+            switch (dateTimeKind)
+            {
+                case "DAY": return getStatistics_Daily(conditionalSql);
+                case "WEEK": return getStatistics_Weekly(conditionalSql);
+                case "MONTH": return getStatistics_Monthly(conditionalSql);
+                default: goto case "DAY";
+            }
+
+        }
+
+        private List<DailyResults> getStatistics_Daily(String? conditionalSql)
+        {
+            List<DailyResults> statisticsDataList = new List<DailyResults>();
+            StringBuilder get = new StringBuilder("select * from dbo.SCAN()");
+            if (conditionalSql != null)
+            {
+                get.Append(conditionalSql);
+            }
+            using (var command = new SqlCommand(get.ToString(), Parameters.sqlConnection))
+            using (var reader = command.ExecuteReader())
+            {
+                /* 時刻リストから、返すデータを作成 */
+                List<Times> times = new List<Times>();
+                while (reader.Read())
+                {
+                    DailyResults columData = createResultsModel(reader, RangeKind.day);
+                    statisticsDataList.Add(columData);
+                }
+            }
+            return statisticsDataList;
+        }
+
+
+        private List<DailyResults> getStatistics_Weekly(String? conditionalSql)
+        {
+            List<DailyResults> statisticsDataList = new List<DailyResults>();
+            StringBuilder get = new StringBuilder("select * from dbo.SCAN()");
+            if (conditionalSql != null)
+            {
+                get.Append(conditionalSql);
+            }
+            using (var command = new SqlCommand(get.ToString(), Parameters.sqlConnection))
+            using (var reader = command.ExecuteReader())
+            {
+                /* 時刻リストから、返すデータを作成 */
+                List<Times> times = new List<Times>();
+                while (reader.Read())
+                {
+                    DailyResults columData = createResultsModel(reader, RangeKind.week);
+                    statisticsDataList.Add(columData);
+                }
+            }
+            return statisticsDataList;
+        }
+
+        private List<DailyResults> getStatistics_Monthly(String? conditionalSql)
+        {
+            List<DailyResults> statisticsDataList = new List<DailyResults>();
+            StringBuilder get = new StringBuilder("select * from dbo.SCAN()");
+            if (conditionalSql != null)
+            {
+                get.Append(conditionalSql);
+            }
+            using (var command = new SqlCommand(get.ToString(), Parameters.sqlConnection))
+            using (var reader = command.ExecuteReader())
+            {
+                /* 時刻リストから、返すデータを作成 */
+                List<Times> times = new List<Times>();
+                while (reader.Read())
+                {
+                    DailyResults columData = createResultsModel(reader, RangeKind.month);
+                    statisticsDataList.Add(columData);
+                }
+            }
+            return statisticsDataList;
+        }
+
+
+
+        private DailyResults createResultsModel(SqlDataReader reader, RangeKind dateRange)
+        {
+            DateTime firstDateOfRange = reader.GetDateTime(0).Date;
+
+
+            DateTime lastDataOfRange = dateRange switch
+            {
+                RangeKind.day => firstDateOfRange,
+                RangeKind.week => firstDateOfRange.AddDays(6),
+                RangeKind.month => firstDateOfRange.AddMonths(1).AddDays(-1),
+                _ => firstDateOfRange
+            };
+
+            return new DailyResults(
+
+                            firstDateOfRange,
+                            lastDataOfRange,
+                            reader.GetInt32(1),
+                            reader.GetInt32(2),
+                            reader.GetInt32(3),
+                            reader.GetInt32(4),
+                            reader.GetInt32(5),
+                            reader.GetInt32(6),
+                            reader.GetInt32(7),
+                            reader.GetInt32(8),
+                            reader.GetInt32(9),
+                            reader.GetInt32(10),
+                            reader.GetInt32(11),
+                            reader.GetInt32(13),
+                            reader.GetInt32(14)
+                );
+        }
+    }
+
+
+
+    public enum RangeKind
+    {
+        day,
+        week,
+        month
+    }
+
+}
---
+++ WebAPISample/JSONModels/DailyResults.cs
@@ -0,0 +1,42 @@
+﻿namespace WebAPISample.JSONModels
+{
+    public class DailyResults
+    {
+        public DateTime firstDateOfRange { get; set; }
+        public DateTime endDateOfRange { get; set; }
+        public int count_Scan { get; set; }
+        public int count_Ok { get; set; }
+
+        public int count_Ng { get; set; }
+
+        public int ngCount_IC1 { get; set; }
+        public int ngCount_IC2 { get; set; }
+        public int ngCount_R5 { get; set; }
+        public int ngCount_R10 { get; set; }
+        public int ngCount_R11 { get; set; }
+        public int ngCount_R12 { get; set; }
+        public int ngCount_R18 { get; set; }
+        public int ngCount_DIPSW { get; set; }
+        public int ngCount_Voltage { get; set; }
+        public int ngCount_Frequency { get; set; }
+
+        public DailyResults(DateTime firstDateOfRange, DateTime endDateOfRange, int count_Scan, int count_Ok, int count_Ng, int ngCount_IC1, int ngCount_IC2, int ngCount_R5, int ngCount_R10, int ngCount_R11, int ngCount_R12, int ngCount_R18, int ngCount_DIPSW, int ngCount_Voltage, int ngCount_Frequency)
+        {
+            this.firstDateOfRange = firstDateOfRange;
+            this.endDateOfRange = endDateOfRange;
+            this.count_Scan = count_Scan;
+            this.count_Ok = count_Ok;
+            this.count_Ng = count_Ng;
+            this.ngCount_IC1 = ngCount_IC1;
+            this.ngCount_IC2 = ngCount_IC2;
+            this.ngCount_R5 = ngCount_R5;
+            this.ngCount_R10 = ngCount_R10;
+            this.ngCount_R11 = ngCount_R11;
+            this.ngCount_R12 = ngCount_R12;
+            this.ngCount_R18 = ngCount_R18;
+            this.ngCount_DIPSW = ngCount_DIPSW;
+            this.ngCount_Voltage = ngCount_Voltage;
+            this.ngCount_Frequency = ngCount_Frequency;
+        }
+    }
+}
